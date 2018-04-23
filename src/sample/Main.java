d448f2fc306e146267f8223303fb239c8ddd89ef
coel16 <modopojken@gmail.com>
2018-04-23 12:00:20 +0200

    Skapade en pad, en boll och gjorde så att dem kan fara runt fritt på skärmen. Sedan la jag till rutor so bollen kan studsa på och ta bort

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -21,12 +21,14 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         JFrame obj = new JFrame();
+        gameplay gamePlay = new gameplay();
         obj.setBounds(10, 10, 700, 600);
         obj.setTitle("Breakout Ball");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
+        obj.add(gamePlay);
+
 
 
     }
