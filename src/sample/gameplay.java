19c4f8bf9e0bdbc74dbea66a05ae60c48be43854
coel16 <modopojken@gmail.com>
2018-05-14 11:09:19 +0200

    La till powerups och återställningsfunktion.

--- src/sample/gameplay.java
+++ src/sample/gameplay.java
@@ -9,8 +9,12 @@ import java.awt.event.KeyListener;
 import javax.swing.Timer;
 
 public class gameplay extends JPanel implements KeyListener, ActionListener {
+
+    //deklaration av alla variabler jag kommer att använda.
+
     private boolean play = false;
     private int score = 0;
+    private int streak = 0;
 
     private int totalBricks = 21;
 
@@ -23,6 +27,11 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
     private int ballposY = 350;
     private int ballXdir = -1;
     private int ballYdir = -2;
+    private int ballwidth = 20;
+    private int ballheight = 20;
+    private int paddlewidth = 100;
+    private int studskoff = paddlewidth/2;
+    
 
     private MapGenerator map;
 
@@ -37,6 +46,7 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
         timer.start();
     }
 
+    //Här rutar jag ut de olika komponenterna av spelfältet
     public void paint(Graphics g){
         //background
         g.setColor(Color.BLACK);
@@ -45,33 +55,58 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
         //Drawing Map
         map.draw((Graphics2D)g);
 
+        //scores
+        g.setColor(Color.WHITE);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString(""+score, 500,30);
+        g.drawString(""+streak, 560, 30);
+
         //borders
-        g.setColor(Color.YELLOW);
+        g.setColor(Color.BLACK);
         g.fillRect(0,0,3,592);
         g.fillRect(0,0,692,3);
         g.fillRect(691,0,3,592);
 
         //the paddle
-        g.setColor(Color.GREEN);
-        g.fillRect(playerX,550,100,8);
+        g.setColor(Color.CYAN);
+        g.fillRect(playerX,550,paddlewidth,8);
 
         //Bollen
-        g.setColor(Color.YELLOW);
-        g.fillOval(ballposX,ballposY,20,20);
+        g.setColor(Color.WHITE);
+        g.fillOval(ballposX,ballposY,ballwidth,ballheight);
 
+        if(ballposY > 570){
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+            g.setColor(Color.RED);
+            g.setFont(new Font("serif",Font.BOLD,30));
+            g.drawString("Du stortorskade, syyyyyyyyyyyyynd",110,300);
+
+            g.setFont(new Font("serif",Font.BOLD,20));
+            g.drawString("Synd, du förlorade. Tryck enter för att starta om!",150,350);
+        }
+        if(totalBricks == 0){
+            play = false;
+
+            g.setColor(Color.RED);
+            g.setFont(new Font("serif",Font.BOLD,30));
+            g.drawString("Alla bricks eleminerade! B",200,300);
+
+            g.setFont(new Font("serif",Font.BOLD,20));
+            g.drawString("Du vann! Grattisen är göda!",200,350);
+
+        }
         g.dispose();
     }
-
-
-
+    //
     @Override
     public void actionPerformed(ActionEvent e) {
         timer.start();
         if(play) {
-            if (new Rectangle(ballposX, ballposY, 20, 20).intersects(new Rectangle(playerX, 550, 100, 8))) {
+            if (new Rectangle(ballposX, ballposY, ballwidth, ballheight).intersects(new Rectangle(playerX, 550, paddlewidth, 8))) {
                 ballYdir = -ballYdir;
             }
-
            A: for(int i = 0; i<map.map.length; i++){
                 for(int j = 0; j<map.map[0].length; j++){
                     if(map.map[i][j] > 0){
@@ -81,7 +116,7 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
                        int brickHeight = map.brickHeight;
 
                        Rectangle rect = new Rectangle(brickX,brickY,brickWidth,brickHeight);
-                       Rectangle ballrect = new Rectangle(ballposX,ballposY,20,20);
+                       Rectangle ballrect = new Rectangle(ballposX,ballposY,ballwidth,ballheight);
                        Rectangle brickRect = rect;
 
                        if(ballrect.intersects(brickRect)){
@@ -94,13 +129,17 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
                            }else{
                                ballYdir = -ballYdir;
                            }
-
+                           if(totalBricks < 10){
+                               ballheight = 40;
+                               ballwidth = 40;
+                           }if(totalBricks < 15){
+                               paddlewidth = 200;
+                           }
                            break A;
                        }
                     }
                 }
             }
-
             ballposX += ballXdir;
             ballposY += ballYdir;
             if(ballposX < 0){
@@ -114,15 +153,13 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
             }
         }
 
-
         repaint();
     }
-
+    //lägger till keylistners för att kunna röra paddan till höger och vänster
     @Override
     public void keyTyped(KeyEvent e) {}
     @Override
     public void keyReleased(KeyEvent e) {}
-
     @Override
     public void keyPressed(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
@@ -139,7 +176,45 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
                 moveLeft();
             }
         }
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            if(!play){
 
+                //Då du har klarat banan eller förlorat så kommer det fram text, då kan du trycka enter för att starta om och denna metod
+                //återställer allt till som det ska vara från början
+
+
+                int maximum;
+                int minimum;
+                int randomNum;
+                int maxatnum;
+                int minstanum;
+                int slumpnum;
+
+                maximum = 400;
+                minimum = 250;
+                maxatnum = 550;
+                minstanum = 50;
+                String mittnr;
+
+                randomNum = minimum + (int) (Math.random()*maximum);
+                slumpnum = minstanum + (int) (Math.random()*maxatnum);
+                play = true;
+                ballposX = slumpnum;
+                ballposY = randomNum;
+                ballXdir = -1;
+                ballYdir = -2;
+                playerX = 310;
+                score = 0;
+                totalBricks = 21;
+                ballwidth = 20;
+                ballheight = 20;
+                paddlewidth = 100;
+                map = new MapGenerator(3,7);
+                streak++;
+
+                repaint();
+            }
+        }
     }
     public void moveRight() {
         play=true;
@@ -149,5 +224,4 @@ public class gameplay extends JPanel implements KeyListener, ActionListener {
         play=true;
         playerX-=20;
     }
-
 }
